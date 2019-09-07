eeab15ae6f2991f133b04ae694250f8b721c6543
Donovan <donovan@Donovan-MBP.fios-router.home>
2019-09-07 16:51:03 -0700

    setting up initial framework

---
+++ config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  siteTitle: 'Donovan', // <title>
+  manifestName: 'Grayscale',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-grayscale/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Donovan Lowkeen',
+  subHeading: 'Full time Web Developer. Part time Open source contributor  ',
+
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/dlowkeen',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/donovanlowkeen/',
+    },
+    {
+      icon: 'fa-instagram',
+      name: 'Instagram',
+      url: 'https://www.instagram.com/highkeylowkeen/',
+    },
+    {
+      icon: 'fa-facebook-f',
+      name: 'Facebook',
+      url: 'https://www.facebook.com/donovan.lowkeen',
+    },
+  ],
+  email: 'dlowkeen@yahoo.com',
+  phone: '000-00000',
+  address: '305 Del Sol, Sunnyvale, CA',
+};
